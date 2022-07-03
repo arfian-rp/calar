04bc6f2f10681ce47460eb789a195ae5adc087bd
arfian-rp <arfianpradana07@gmail.com>
2022-07-03 17:13:17 +0700

    v1.1.0

--- main.go
+++ main.go
@@ -31,33 +31,39 @@ github			: https://github.com/arfian-rp/calar`)
 	case "init":
 		CreateInit(pwd)
 	default:
-		jsonData, err := ioutil.ReadFile(pwd + filename)
-		if err != nil {
-			panic(err)
-		}
-		var setup map[string]string
-		json.Unmarshal(jsonData, &setup)
-		com := strings.Fields(setup[args[1]])
-		out, err2 := exec.Command(com[0], com[1:]...).Output()
-		if err2 != nil {
-			panic(err2)
-		}
-		fmt.Println(string(out))
+		fmt.Println(RunScript(pwd, args))
 	}
 
 }
 func CreateInit(pwd string) {
-	f, err := os.Create(pwd + filename)
+	if _, err := os.Stat("/path/to/whatever"); err != nil {
+		f, err := os.Create(pwd + filename)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		_, err2 := f.WriteString(`{
+"start":"echo run start"
+}`)
+		if err2 != nil {
+			panic(err2)
+		}
+		fmt.Println("done")
+	}
+
+}
+func RunScript(pwd string, args []string) string {
+	jsonData, err := ioutil.ReadFile(pwd + filename)
 	if err != nil {
 		panic(err)
 	}
-	defer f.Close()
-
-	_, err2 := f.WriteString(`{
-	"start":"echo run start"
-}`)
+	var setup map[string]string
+	json.Unmarshal(jsonData, &setup)
+	com := strings.Fields(setup[args[1]])
+	out, err2 := exec.Command(com[0], com[1:]...).Output()
 	if err2 != nil {
 		panic(err2)
 	}
-	fmt.Println("done")
+	return string(out)
 }
